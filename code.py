d2b2cf98afb0cdb5499e8849c039d05cd09ca697
blablaablaa <162125609+blablaablaa@users.noreply.github.com>
2024-03-10 01:10:47 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,9 @@ with open('C:\\Users\\Vikram\\Desktop\\CO_TEST.txt') as f:
     l = []
     for i in list:
         l.append((i.rstrip("\n").split()))
-print(l)
+with open('binary.txt','w') as f:
+    f.write("")
+# print(l)
 
 R_type = ["add", "sub", "slt", "sltu", "xor", "sll", "srl", "or", "and"]
 I_type = ["lb", "lh", "lw", "ld", "addi", "sltiu", "jalr"]
@@ -49,38 +51,30 @@ regABItoBinary = {"zero": "00000",
                   }
 
 VH = False
-
-
-def decimal_to_b11(decimal_number):
-    if decimal_number < -2 ** 11 or decimal_number >= 2 ** 11:
-        raise ValueError("Decimal number out of range for 12 bits representation")
-    if decimal_number < 0:
-        decimal_number = 2 ** 12 + decimal_number
-    binary_representation = bin(decimal_number)[2:]
-    padded_binary = binary_representation.zfill(12)
-    return str(padded_binary)
-
-
-def decimal_to_b20(decimal_number):
-    if decimal_number < -2 ** 19 or decimal_number >= 2 ** 19:
-        raise ValueError("Decimal number out of range for 20 bits representation")
-    if decimal_number < 0:
-        decimal_number = 2 ** 20 + decimal_number
-    binary_representation = bin(decimal_number)[2:]
-    padded_binary = binary_representation.zfill(20)
-    return str(padded_binary)
-
-
+# def decimal_to_b11(decimal_number):
+#     if decimal_number < -2 ** 11 or decimal_number >= 2 ** 11:
+#         raise ValueError("Decimal number out of range for 12 bits representation")
+#     if decimal_number < 0:
+#         decimal_number = 2 ** 12 + decimal_number
+#     binary_representation = bin(decimal_number)[2:]
+#     padded_binary = binary_representation.zfill(12)
+#     return str(padded_binary)
+# def decimal_to_b20(decimal_number):
+#     if decimal_number < -2 ** 19 or decimal_number >= 2 ** 19:
+#         raise ValueError("Decimal number out of range for 20 bits representation")
+#     if decimal_number < 0:
+#         decimal_number = 2 ** 20 + decimal_number
+#     binary_representation = bin(decimal_number)[2:]
+#     padded_binary = binary_representation.zfill(20)
+#     return str(padded_binary)
 def decimal_to_b32(decimal_number):
     if decimal_number < -2 ** 31 or decimal_number >= 2 ** 31:
-        raise ValueError("Decimal number out of range for 12 bits representation")
+        raise ValueError("Decimal number out of range for 32 bits representation")
     if decimal_number < 0:
         decimal_number = 2 ** 32 + decimal_number
     binary_representation = bin(decimal_number)[2:]
     padded_binary = binary_representation.zfill(32)
     return str(padded_binary)
-
-
 def getInstructionType(ins):
     if ins in R_type:
         return "R"
@@ -112,12 +106,18 @@ def convertR(instruction):
     rd = temp[0]
     rs1 = temp[1]
     rs2 = temp[2]
+    try:
+        regABItoBinary[rs2]
+        regABItoBinary[rs1]
+        regABItoBinary[rd]
+    except:
+        return -1
 
     if instruction[0] == "sub":
-        return (f'0100000{regABItoBinary[rs2]} {regABItoBinary[rs1]} {funct3[instruction[0]]}'
-                f' {regABItoBinary[rd]} {opcode}')
-    return (f'0000000{regABItoBinary[rs2]} {regABItoBinary[rs1]} {funct3[instruction[0]]} '
-            f'{regABItoBinary[rd]} {opcode}')
+        return (f'0100000{regABItoBinary[rs2]}{regABItoBinary[rs1]}{funct3[instruction[0]]}'
+                f'{regABItoBinary[rd]}{opcode}')
+    return (f'0000000{regABItoBinary[rs2]}{regABItoBinary[rs1]}{funct3[instruction[0]]}'
+            f'{regABItoBinary[rd]}{opcode}')
 
 
 def convertI(instruction):
@@ -136,17 +136,28 @@ def convertI(instruction):
         temp = instruction[1].split(',')
         rd = temp[0]
         rs = temp[1]
+
+        try:
+            regABItoBinary[rs]
+            regABItoBinary[rd]
+        except:
+            return -1
         imm = temp[2]
         imm_b = decimal_to_b32(int(imm))
-        return f'{imm_b[20:]} {regABItoBinary[rs]} {funct3[ins_name]} {regABItoBinary[rd]} {opcode[ins_name]}'
+        return f'{imm_b[20:]}{regABItoBinary[rs]}{funct3[ins_name]}{regABItoBinary[rd]}{opcode[ins_name]}'
     else:
         temp = instruction[1].split(',')
         temp2 = temp[1].split('(')
         rd = temp[0]
         rs = temp2[1].rstrip(')')
+        try:
+            regABItoBinary[rs]
+            regABItoBinary[rd]
+        except:
+            return -1
         imm = temp2[0]
         imm_b = decimal_to_b32(int(imm))
-        return f'{imm_b[20:]} {regABItoBinary[rs]} {funct3[ins_name]} {regABItoBinary[rd]} {opcode[ins_name]}'
+        return f'{imm_b[20:]}{regABItoBinary[rs]}{funct3[ins_name]}{regABItoBinary[rd]}{opcode[ins_name]}'
 
 
 def convertS(instruction):
@@ -157,12 +168,17 @@ def convertS(instruction):
     temp2 = temp[1].split('(')
     rs2 = temp[0]
     rs1 = temp2[1].rstrip(')')
+    try:
+        regABItoBinary[rs1]
+        regABItoBinary[rs2]
+    except:
+        return -1
     imm = temp2[0]
-    imm_b = decimal_to_b11(int(imm))
-    return f'{imm_b[0:7]} {regABItoBinary[rs2]} {regABItoBinary[rs1]} {funct3[ins_name]} {imm_b[7:]} {opcode[ins_name]}'
+    imm_b = decimal_to_b32(int(imm))
+    return f'{imm_b[20:27]}{regABItoBinary[rs2]}{regABItoBinary[rs1]}{funct3[ins_name]}{imm_b[27:]}{opcode[ins_name]}'
 
 
-def convertB(instruction):
+def convertB(instruction, index):
     opcode = "1100011"
     funct3 = {"beq": "000",
               "bne": "001",
@@ -177,11 +193,16 @@ def convertB(instruction):
         imm_b = decimal_to_b32(int(imm))
     except:
         val = label[imm]
-        imm = (val - index)*4
+        imm = (index-val)*4
         # print(imm)
         imm_b = decimal_to_b32(int(imm))
-    return (f'{imm_b[19]} {imm_b[21:27]} {regABItoBinary[rs2]} {regABItoBinary[rs1]} {funct3[ins_name]}'
-            f' {imm_b[27:31]} {imm_b[20]} {opcode}')
+    try:
+        regABItoBinary[rs1]
+        regABItoBinary[rs2]
+    except:
+        return -1
+    return (f'{imm_b[19]}{imm_b[21:27]}{regABItoBinary[rs2]}{regABItoBinary[rs1]}{funct3[ins_name]}'
+            f'{imm_b[27:31]}{imm_b[20]}{opcode}')
 
 
 def convertU(instruction):
@@ -189,31 +210,38 @@ def convertU(instruction):
               "auipc": "0010111"}
     ins_name = instruction[0]
     rd, imm = instruction[1].split(',')
+    try:
+        regABItoBinary[rd]
+    except:
+        return -1
     imm_b = decimal_to_b32(int(imm))
-    return f'{imm_b[:20]} {regABItoBinary[rd]} {opcode[ins_name]}'
+    return f'{imm_b[:20]}{regABItoBinary[rd]}{opcode[ins_name]}'
 
 def convertJ(instruction, index):
-    opcode = "1100111"
+    opcode = "1101111"
     rd,imm = instruction[1].split(',')
     try:
         imm_b = decimal_to_b32(int(imm))
     except:
         val = label[imm]
-        imm = (val - index)*4
-        # print(imm)
+        imm = (index - val)*4
         imm_b = decimal_to_b32(int(imm))
-    return f'{imm_b[11]} {imm_b[21:31]} {imm_b[20]} {imm_b[12:20]} {regABItoBinary[rd]} {opcode}'
+    try:
+        regABItoBinary[rd]
+    except:
+        return -1
+    return f'{imm_b[11]}{imm_b[21:31]}{imm_b[20]}{imm_b[12:20]}{regABItoBinary[rd]}{opcode}'
 
 
 l = [instruction for instruction in l if instruction]
-
 for address_instruction in range(len(l)):
     instruct = l[address_instruction]
-    if len(instruct) == 3:
-        x = (instruct[0])[:-1]
+    if ":" in instruct[0]:
+        lab = instruct[0][:-1]
+        label[lab] = address_instruction
         l[address_instruction] = instruct[1:]
-        label[x] = address_instruction
-
+# print(l)
+l = [instruction for instruction in l if instruction]
 for index in range(len(l)):
     instruction = l[index]
     ins_type = getInstructionType(instruction[0])
@@ -224,7 +252,7 @@ for index in range(len(l)):
     elif ins_type == "S":
         s = convertS(instruction)
     elif ins_type == "B":
-        s = convertB(instruction)
+        s = convertB(instruction,index)
         if instruction == ["beq", "zero,zero,0"]:
             VH = True
     elif ins_type == "U":
@@ -232,17 +260,31 @@ for index in range(len(l)):
     elif ins_type == "J":
         s = convertJ(instruction,index)
     else:
-        print(instruction[0])
+        # print(instruction[0])
         with open("binary.txt", mode='w') as f:
-            f.write("ERROR1")
+            # print(instruction)
+            print("ERROR..Invalid Instruction name")
+            f.write("ERROR..Invalid Instruction name")
         break
-    print("adding....", end=" ")
-    print(" ".join(instruction))
+    # print("adding....", end=" ")
+    # print(" ".join(instruction))
+    if s==-1:
+        with open("binary.txt", mode='w') as f:
+
+            print("ERROR..Invalid ABI register name")
+            f.write("ERROR..Invalid ABI register name")
+        break
     with open("binary.txt", mode='a') as f:
         f.write(s + "\n")
-
-if not VH:
-    with open("binary.txt", mode='w') as f:
-        f.write("ERROR")
 else:
-    pass
+    if not VH:
+        with open("binary.txt", mode='w') as f:
+            print("ERROR..Virtual Halt not present")
+            f.write("ERROR..Virtual Halt not present")
+    else:
+        with open('binary.txt', 'r') as f:
+            l = f.readlines()
+            l[-1] = l[-1].rstrip("\n")
+            s = "".join(l)
+        with open('binary.txt', 'w') as f:
+            f.write(s)
